d341aa519358bb7a39cf5a52016fb3203ab000b5
Gtee2311 <gtee@hotmail.my>
2015-05-28 17:12:39 +0800

    Upgrade: app to have revocable session token

--- app/src/main/java/com/gracetee/meeterholic/Meeterholic.java
+++ app/src/main/java/com/gracetee/meeterholic/Meeterholic.java
@@ -2,6 +2,7 @@ package com.gracetee.meeterholic;
 
 import android.app.Application;
 import com.parse.Parse;
+import com.parse.ParseUser;
 
 /**
  * Created by Grace on 24/5/2015.
@@ -11,6 +12,7 @@ public class Meeterholic extends Application {
     public void onCreate() {
         super.onCreate();
         Parse.initialize(this, "zSK1a4B3BvTM25OX53fv0dL4k2VyVh9qQY1gfiHF", "70ILY4Tja4EKFBkasOEDZQjtwxd94LxxPKXV0r5H");
+        ParseUser.enableRevocableSessionInBackground();
     }
 
 }
